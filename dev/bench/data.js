5c97077d571fda17f840eb6c126246ce32455d0e
github-action-benchmark <github@users.noreply.github.com>
2025-08-06 07:44:34 +0000

    add My test benchmark results (customSmallerIsBetter) benchmark result for b8f77fe11e3e4c20f3e99b1790f4a72095ad5e41

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754466170541,
+  "lastUpdate": 1754466274964,
   "repoUrl": "https://github.com/cyril0124/test-ci",
   "entries": {
     "My test benchmark results": [
@@ -94,6 +94,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "My Custom Smaller Is Better Benchmark - CPU Load",
             "value": 70,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "cyril0124",
+            "username": "cyril0124",
+            "email": "211998078@qq.com"
+          },
+          "committer": {
+            "name": "cyril0124",
+            "username": "cyril0124",
+            "email": "211998078@qq.com"
+          },
+          "id": "b8f77fe11e3e4c20f3e99b1790f4a72095ad5e41",
+          "message": "update",
+          "timestamp": "2025-08-06T07:13:54Z",
+          "url": "https://github.com/cyril0124/test-ci/commit/b8f77fe11e3e4c20f3e99b1790f4a72095ad5e41"
+        },
+        "date": 1754466274614,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
             "unit": "Percent"
           },
           {
