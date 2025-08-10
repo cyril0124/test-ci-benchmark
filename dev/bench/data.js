a6264915f7db218cf847d0649898489568059e2d
github-action-benchmark <github@users.noreply.github.com>
2025-08-10 02:41:59 +0000

    add My test benchmark results (customSmallerIsBetter) benchmark result for ad1989a740dfc12be9cfb5d7431ea262e9556e3f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754466274964,
+  "lastUpdate": 1754793719879,
   "repoUrl": "https://github.com/cyril0124/test-ci",
   "entries": {
     "My test benchmark results": [
@@ -125,6 +125,54 @@ window.BENCHMARK_DATA = {
         "date": 1754466274614,
         "tool": "customSmallerIsBetter",
         "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "211998078@qq.com",
+            "name": "cyril0124",
+            "username": "cyril0124"
+          },
+          "committer": {
+            "email": "211998078@qq.com",
+            "name": "cyril0124",
+            "username": "cyril0124"
+          },
+          "distinct": true,
+          "id": "ad1989a740dfc12be9cfb5d7431ea262e9556e3f",
+          "message": "update",
+          "timestamp": "2025-08-10T10:40:39+08:00",
+          "tree_id": "4d2ec16d7f2b7f4711a3db45e14b92e44bb4528a",
+          "url": "https://github.com/cyril0124/test-ci/commit/ad1989a740dfc12be9cfb5d7431ea262e9556e3f"
+        },
+        "date": 1754793719435,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Test",
+            "value": 100,
+            "unit": "ms",
+            "extra": "type 1"
+          },
+          {
+            "name": "Test1",
+            "value": 200,
+            "unit": "ms",
+            "extra": "type 2"
+          },
           {
             "name": "My Custom Smaller Is Better Benchmark - CPU Load",
             "value": 50,
